bb9007363af685768d8e4eeee58a1e953a031045
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2025-09-05 18:35:02 +0000

    added a prompt

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ import { TextInterface } from "text-interface";
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
 let ti = new TextInterface(app, "Example Text Interface");
-
+ti.promptNumber("What is your favorite number?");
+let favoriteNumber = await ti.readNumber();
 ti.output("What is your name?");
 let name = await ti.readText();
-await ti.output("Hello, " + name);
-
-
+if (name === "Alice" || name === "Bob") {
+  ti.output(`Hello, ${name}!`);
+}
+else if (name > "M") {
+  ti.output("Wow your name is late in the alphabet");
+} else {
+  ti.output("What a pretty name!");
+}
